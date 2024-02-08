96d637a1e8ee27924a6b1f2060efd063a6e7e178
RaziaAkbari <akbariharis14@gmail.com>
2024-02-08 15:40:44 +0430

    titles formatted

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,7 +3,8 @@ import React from "react"
 function Contact() {
   return (
     <div className=" container p-4 flex flex-wrap items-center justify-between mx-auto">
-      Contact Us
+        <strong>Contact Us</strong>
+      
       <br />
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum minus
       tempore architecto esse natus veniam earum ad, laborum atque quibusdam
--- src/Pages/Programs.jsx
+++ src/Pages/Programs.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Programs() {
   return (
     <div className=" container p-4 flex flex-wrap items-center justify-between mx-auto">
-      Programs
+      <strong>Programs </strong>
       <br />
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum minus
       tempore architecto esse natus veniam earum ad, laborum atque quibusdam
